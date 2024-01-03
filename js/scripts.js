89bf428a13fb3bf466061ea899067c4b07a9c7e1
Minuka hansaja <108428943+Minuka2022@users.noreply.github.com>
2024-01-03 17:06:22 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1178,6 +1178,7 @@ $(document).ready(function(){
 
 
 
+
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -1187,4 +1188,3 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-
